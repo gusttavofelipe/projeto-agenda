5cbdded002ed1e302a67461e78bd2e24444e0799
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-10-11 20:30:00 -0300

    usando condicionais

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from .models import Categoria, Contato # importando models
 
 class ContadoAdmin(admin.ModelAdmin):
-    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', ) # exibindo informações em contato
+    list_display = ('id','nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar' ) # exibindo informações em contato
     list_display_links = ('id', 'nome',) # adiciona um link no campo desejado
     list_filter = ('nome', 'sobrenome', ) # adiciona um filtro com os campos desejados
     list_per_page = 10 # define quantos elemetos por tabela serão exbidos por pagina
     search_fields = ('id', 'nome', 'sobrenome') # adiciona à area de pesquisa os campos desejados
+    list_editable = ('telefone', 'mostrar') # tornando campos listados editaveis/interativos
 
 class CategoriaAdmin(admin.ModelAdmin):
    pass
--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,4 @@
+from xml.etree.ElementInclude import default_loader
 from django.db import models
 from django.utils import timezone
 
@@ -14,7 +15,8 @@ class Contato(models.Model):
     email = models.CharField(max_length=255, blank=True) # adicionando campo email
     data_criacao = models.DateTimeField(default=timezone.now) # adicionando campo data da criação
     descricao =models.TextField(blank=True) # adicionando campo descrição
-    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING) # # adicionando categoria a ser selecionada
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING) # adicionando categoria a ser selecionada
+    mostrar = models.BooleanField(default=True) # campo de alteraração de visualização de um contato
 
     def __str__(self) -> str: # representando objetos pelo nome(no site)
         return self.nome
