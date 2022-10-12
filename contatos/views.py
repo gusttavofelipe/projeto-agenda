0dc6ae9a4e7bbba465d27bbc664135477e60b671
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-10-12 17:53:22 -0300

    paginação

--- contatos/views.py
+++ contatos/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from contatos.models import Contato
 
 def index(request):
     contatos = Contato.objects.all()
+    paginador = Paginator(contatos, 7) # numero de contatos por pagina
+
+    num_paginas = request.GET.get('p') # variavel que contem numeração das paginas
+    contatos = paginador.get_page(num_paginas)
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
-
+                                       
 def detalhes(request, contato_id): 
     detalhes = get_object_or_404(Contato, id=contato_id) # levantando erro 404
     return render(request, 'contatos/detalhes.html', { 
