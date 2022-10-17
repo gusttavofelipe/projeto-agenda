2da26c585fabab605217b56d0168f7b58816a94f
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-10-17 19:57:31 -0300

    ordenando e filtrando valores

--- contatos/views.py
+++ contatos/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from contatos.models import Contato
+from django.http import Http404
 
-def index(request):
-    contatos = Contato.objects.all()
+def index(request):            # ordenando por id decrescente
+    contatos = Contato.objects.order_by('-id').filter(
+        mostrar=True # filtrando pela variavel quando for verdadeira
+    )
     paginador = Paginator(contatos, 7) # numero de contatos por pagina
 
     num_paginas = request.GET.get('p') # variavel que contem numeração das paginas
@@ -14,6 +17,10 @@ def index(request):
                                        
 def detalhes(request, contato_id): 
     detalhes = get_object_or_404(Contato, id=contato_id) # levantando erro 404
+
+    if not detalhes.mostrar: 
+        raise Http404
+
     return render(request, 'contatos/detalhes.html', { 
         'detalhe': detalhes
     })
