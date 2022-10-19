e74be702bf969b39f196e7399d77feb7aeb4c3b3
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2022-10-19 18:28:02 -0300

    campo de pesquisa

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:contato_id>', views.detalhes, name='detalhes') 
-]   # mandando argumento nomeado e tipado para views
+    path('<int:contato_id>', views.detalhes, name='detalhes') ,
+     # mandando argumento nomeado e tipado para views
+    path('busca/', views.busca, name='busca') 
+]  
--- contatos/views.py
+++ contatos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models.functions import Concat
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
 from contatos.models import Contato
 from django.http import Http404
 
@@ -15,6 +17,7 @@ def index(request):            # ordenando por id decrescente
         'contatos': contatos
     })
                                        
+
 def detalhes(request, contato_id): 
     detalhes = get_object_or_404(Contato, id=contato_id) # levantando erro 404
 
@@ -23,4 +26,26 @@ def detalhes(request, contato_id):
 
     return render(request, 'contatos/detalhes.html', { 
         'detalhe': detalhes
+    })
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    campos = Concat('nome', Value(' '), 'sobrenome') # Value simula um campo no db
+
+    if termo is None:
+        raise Http404() 
+
+    contatos = Contato.objects.annotate(
+        nome_completo=campos).filter(
+            Q(nome_completo__icontains=termo) | Q(telefone__icontains=termo)
+    )       # pesquisando parcialmente nome e sobrenome e telefone
+
+    # print(contatos.query)
+    paginador = Paginator(contatos, 7) # numero de contatos por pagina
+
+    num_paginas = request.GET.get('p') # variavel que contem numeração das paginas
+    contatos = paginador.get_page(num_paginas)
+    return render(request, 'contatos/busca.html', {
+        'contatos': contatos
     })
